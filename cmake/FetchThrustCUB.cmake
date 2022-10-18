bd787b938bdf6a5cc23e15c7a6e1a790045a1b94
Atharva Gondhalekar <agondhal@amd.com>
2022-10-18 11:48:05 -0400

    Use the available versions of rocthrust and hipcub

--- cmake/FetchThrustCUB.cmake
+++ cmake/FetchThrustCUB.cmake
@@ -1,23 +1,25 @@
 include(FetchContent)
 set(FETCHCONTENT_QUIET ON)
 
-message(STATUS "Cloning External Project: Thrust")
-get_filename_component(FC_BASE "${PROJECT_SOURCE_DIR}/externals"
-                REALPATH BASE_DIR "${CMAKE_BINARY_DIR}")
-set(FETCHCONTENT_BASE_DIR ${FC_BASE})
+#message(STATUS "Cloning External Project: Thrust")
+#get_filename_component(FC_BASE "../externals"
+#                REALPATH BASE_DIR "${CMAKE_BINARY_DIR}")
+#set(FETCHCONTENT_BASE_DIR ${FC_BASE})
 
-FetchContent_Declare(
-    thrust
-    GIT_REPOSITORY https://github.com/thrust/thrust.git
-    GIT_TAG        1.17.0
-)
+#FetchContent_Declare(
+#    thrust
+#    GIT_REPOSITORY https://github.com/thrust/thrust.git
+#    GIT_TAG        1.17.0
+#)
 
-FetchContent_GetProperties(thrust)
-if(NOT thrust_POPULATED)
-  FetchContent_Populate(
-    thrust
-  )
-endif()
-set(THRUST_INCLUDE_DIR "${thrust_SOURCE_DIR}")
+#FetchContent_GetProperties(thrust)
+#if(NOT thrust_POPULATED)
+#  FetchContent_Populate(
+#    thrust
+#  )
+#endif()
+#set(THRUST_INCLUDE_DIR "${thrust_SOURCE_DIR}")
+set(THRUST_INCLUDE_DIR "${/opt/rocm-5.2.3/rocthrust/include}")
 # Windows doesn't support symblink, so make sure we link to the real library.
-set(CUB_INCLUDE_DIR "${thrust_SOURCE_DIR}/dependencies/cub")
+#set(CUB_INCLUDE_DIR "${thrust_SOURCE_DIR}/dependencies/cub")
+set(CUB_INCLUDE_DIR "${/opt/rocm-5.2.3/hipcub/include/hipcub}")
